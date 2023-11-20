2402e90c5e41c88895f7627474e26232d5f557b6
lim6112j <lim6112j@gmail.com>
2023-11-20 23:38:20 +0900

    initial commit

---
+++ scenes/ui/arena_time_ui.gd
@@ -0,0 +1,9 @@
+extends CanvasLayer
+
+@export var arena_time_manager: Node
+@onready var label = $%Label
+func _process(delta):
+	if arena_time_manager == null:
+		return
+	var time_elapsed = arena_time_manager.get_time_elapsed()
+	label.text = str(time_elapsed)
