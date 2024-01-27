da08b441ed264c60417f866f635d224837f7644d
ben lim <lim6112j@gmail.com>
2024-01-27 12:54:23 +0900

    20240127

--- scenes/ui/arena_time_ui.gd
+++ scenes/ui/arena_time_ui.gd
@@ -6,4 +6,8 @@ func _process(delta):
 	if arena_time_manager == null:
 		return
 	var time_elapsed = arena_time_manager.get_time_elapsed()
-	label.text = str(time_elapsed)
+	label.text = format_seconds(time_elapsed)
+func format_seconds(seconds: float):
+	var minutes = floor(seconds/60)
+	var remaining_seconds = seconds - minutes * 60
+	return str(minutes) + ":" +"%02d" % floor(remaining_seconds)
