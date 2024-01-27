ce6841a49098281013f80d38272482a15ef0a1c4
ben lim <lim6112j@gmail.com>
2024-01-27 15:44:32 +0900

    implement damage

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -3,9 +3,6 @@ extends CharacterBody2D
 const MAX_SPEED = 75
 # Called when the node enters the scene tree for the first time.
 @onready var health_component: HealthComponent = $HealthComponent
-func _ready():
-	$Area2D.area_entered.connect(on_area_entered)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -19,8 +16,4 @@ func get_direction_to_player():
 	if player_node != null:
 		return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO
-func on_area_entered(other_area: Area2D):
-	health_component.damage(100)
-	if health_component.current_health == 0:
-		queue_free()
 	
