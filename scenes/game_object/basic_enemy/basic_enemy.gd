45b414fbeaf63bf8ecfe61db6890e7735d27f5e9
ben lim <lim6112j@gmail.com>
2024-01-27 14:39:16 +0900

    creating health conmponent

--- scenes/game_object/basic_enemy/basic_enemy.gd
+++ scenes/game_object/basic_enemy/basic_enemy.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 const MAX_SPEED = 75
 # Called when the node enters the scene tree for the first time.
+@onready var health_component: HealthComponent = $HealthComponent
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
 
@@ -19,5 +20,7 @@ func get_direction_to_player():
 		return (player_node.global_position - global_position).normalized()
 	return Vector2.ZERO
 func on_area_entered(other_area: Area2D):
-	queue_free()
+	health_component.damage(100)
+	if health_component.current_health == 0:
+		queue_free()
 	
