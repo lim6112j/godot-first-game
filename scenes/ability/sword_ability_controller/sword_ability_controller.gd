56e2e2d677191246363e8d0045518de99c4928e9
lim6112j <lim6112j@gmail.com>
2024-01-30 18:04:24 +0900

    making upgrade functional 2

--- scenes/ability/sword_ability_controller/sword_ability_controller.gd
+++ scenes/ability/sword_ability_controller/sword_ability_controller.gd
@@ -2,17 +2,15 @@ extends Node2D
 const MAX_RANGE = 150
 @export var sword_ability: PackedScene
 var damage = 5
+var base_wait_time
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Timer.timeout.connect(on_timer_timeout)
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+	GameEvents.ability_upgrade_added.connect(on_ability_upgrade_added)
 
 func on_timer_timeout():
+	base_wait_time = $Timer.wait_time
 	var player = get_tree().get_first_node_in_group("player") as Node2D
 	if player == null:
 		return
@@ -35,3 +33,9 @@ func on_timer_timeout():
 	sword_instance.global_position += Vector2.RIGHT.rotated(randf_range(0, TAU)) * 4
 	var enemy_direction = enemies[0].global_position - sword_instance.global_position
 	sword_instance.rotation = enemy_direction.angle()
+func on_ability_upgrade_added(upgrade: AbilityUpgrade, current_upgrades: Dictionary):
+	if upgrade.id != "sword_rate":
+		return
+	var percent_reduction = current_upgrades["sword_rate"]["quantity"] * .1
+	$Timer.wait_time = base_wait_time * (1 - percent_reduction)
+	$Timer.start()
